25291710e232b11bd9dc092b976c85a826c52fe4
harith01 <harithyusuph@gmail.com>
2023-06-23 14:57:38 +0000

    Fix error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# creates a file in /tmp
+
 file { '/tmp/school':
     ensure  => present,
     mode    => '0744',
-    owner   => 'www-data'
+    owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
-    }
+}
